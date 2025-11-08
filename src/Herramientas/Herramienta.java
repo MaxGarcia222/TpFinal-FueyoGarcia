14510a0fee1035e9799f44e2febfe7dd7328ab62
Maximo Manuel Garcia <mabsiaoficial@gmail.com>
2025-11-08 16:10:44 -0300

    .

--- src/Herramientas/Herramienta.java
+++ src/Herramientas/Herramienta.java
@@ -2,6 +2,6 @@ package Herramientas;
 
 public abstract class Herramienta {
     private String nombre;
-    private String descripcion;    //Que hace masomenos, hola mabsia
+    private String descripcion;    //Que hace masomenos, hola mabsia, hola gordita
     private boolean requierePremium;
 }
